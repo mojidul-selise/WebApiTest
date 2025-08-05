86d7604f4f19ee0c3c501ebe800521240509dfbe
Mojidul Islam <md.mojidul@selisegroup.com>
2025-08-05 16:51:26 +0600

    Add project files.

---
+++ WebApiVersioning/Controllers/v2/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using WebApiVersioning.Models;
+
+namespace WebApiVersioning.Controllers.v2
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/weather")]
+    [ApiVersion("2.0")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public async Task<ServiceResponseModel> Get()
+        {
+            var response = new ServiceResponseModel();
+            var data = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+           .ToArray();
+            response.SuccessResponse("Data Get from Version 2.0", data);
+            return response;
+        }
+    }
+}
---
+++ WebApiVersioning/Models/ServiceResponseModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApiVersioning.Models
+{
+    public class ServiceResponseModel
+    {
+        public bool IsSuccess { get; set; }
+        public string?  Message { get; private set; }
+        public dynamic? Data { get; private set; }
+
+        public void SuccessResponse(string? message, dynamic? data) {
+            this.IsSuccess = true;
+            this.Message = message;
+            this.Data = data;
+        }
+    }
+}
